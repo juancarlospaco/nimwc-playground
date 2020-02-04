1abdfafbbe8543d4187a8daa526d0f01b7543281
Juan Carlos <juancarlospaco@gmail.com>
2020-02-04 02:38:44 -0300

    update

--- playground.nim
+++ playground.nim
@@ -1,10 +1,29 @@
-import strutils, db_sqlite
+import strutils, db_sqlite, json
 include "index.nimf"       # Include the NimF template.
 template playgroundStart*(db: DbConn) = exec(db, sql"""
   create table if not exists playground(
     id             integer         primary key,
+    creation       timestamp       not null    default (strftime('%s', 'now')),
     title          varchar(25)     not null,
-    description    varchar(300)    not null,
     email          varchar(254)    not null,
-    creation       timestamp       not null    default (strftime('%s', 'now'))
+    code           varchar(999)    not null,
+    comment        varchar(99)     not null,
+    target         varchar(9)      not null,
+    mode           varchar(9)      not null,
+    gc             varchar(9)      not null,
+    stylecheck     varchar(9)      not null,
+    exceptions     varchar(9)      not null,
+    os             varchar(9)      not null,
+    cpu            varchar(9)      not null,
+    ssl            integer         not null    default 0,
+    threads        integer         not null    default 0,
+    strip          integer         not null    default 1,
+    python         integer         not null    default 0,
+    flto           integer         not null    default 0,
+    fastmath       integer         not null    default 0,
+    hardened       integer         not null    default 0,
+    fontsize       integer         not null    default 12,
+    fontfamily     varchar(9)      not null,
+    expiration     integer         not null    default 7,
+    stdin          varchar(99)     not null
   );  """)                 # Create Playground DB Table.
--- routes.nim
+++ routes.nim
@@ -2,4 +2,90 @@
     resp genPlayground()
 
   post "/compile":
-    resp $request
+    if tryExec(db, sql"""insert into playground(
+      title,email,code,comment,target,mode,gc,stylecheck,exceptions,os,cpu,ssl,
+      threads,strip,python,flto,fastmath,hardened,fontsize,fontfamily,expiration,stdin
+    ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+      @"title".normalize,
+      @"code".strip,
+      @"commentinput".strip,
+      @"target".normalize,
+      @"mode".normalize,
+      @"gc".normalize,
+      @"stylecheck".normalize,
+      @"exceptions".normalize,
+      @"os".normalize,
+      @"cpu".normalize,
+      if @"ssl" == "on": 1 else: 0,
+      if @"threads" == "on": 1 else: 0,
+      if @"threads" == "on": 1 else: 0,
+      if @"strip" == "on": 1 else: 0,
+      if @"python" == "on": 1 else: 0,
+      if @"flto" == "on": 1 else: 0,
+      if @"fastmath" == "on": 1 else: 0,
+      if @"hardened" == "on": 1 else: 0,
+      @"fontsize".parseInt.int8.Positive,
+      @"fontfamily".strip,
+      @"expiration".parseInt.int8.Positive,
+      @"stdin".strip,
+    ):
+      echo ${
+        "code": @"code",
+        "stats": @"stats",
+        "commentoutput": @"commentoutput",
+        "commentinput": @"commentinput",
+        "target": @"target",
+        "mode": @"mode",
+        "gc": @"gc",
+        "stylecheck": @"stylecheck",
+        "exceptions": @"exceptions",
+        "os": @"os",
+        "cpu": @"cpu",
+        "ssl": @"ssl",
+        "threads": @"threads",
+        "strip": @"strip",
+        "python": @"python",
+        "flto": @"flto",
+        "fastmath": @"fastmath",
+        "hardened": @"hardened",
+        "fontsize": @"fontsize",
+        "fontfamily": @"fontfamily",
+        "title": @"title",
+        "expiration": @"expiration",
+        "stdin": @"stdin"
+      }
+    else:
+      resp "FAIL"
+    when not defined(release):
+      echo ${
+        "code": @"code",
+        "stats": @"stats",
+        "commentoutput": @"commentoutput",
+        "commentinput": @"commentinput",
+        "target": @"target",
+        "mode": @"mode",
+        "gc": @"gc",
+        "stylecheck": @"stylecheck",
+        "exceptions": @"exceptions",
+        "os": @"os",
+        "cpu": @"cpu",
+        "ssl": @"ssl",
+        "threads": @"threads",
+        "strip": @"strip",
+        "python": @"python",
+        "flto": @"flto",
+        "fastmath": @"fastmath",
+        "hardened": @"hardened",
+        "fontsize": @"fontsize",
+        "fontfamily": @"fontfamily",
+        "title": @"title",
+        "expiration": @"expiration",
+        "stdin": @"stdin"
+      }
+
+
+# strip code
+# autoformat code
+# save code to file
+# compile code via firejails
+# redirect to the same page with code
