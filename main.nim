23faed042754e568cea8fb6c51dc26142531b7b6
juancarlospaco <juancarlospaco@gmail.com>
2020-02-19 12:24:42 -0300

    More awesome than ever

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import strutils, db_sqlite, json, strtabs, os, osproc, random, jester, net, times, packages/docutils/rstgen
+import strutils, db_sqlite, json, strtabs, os, osproc, random, net, times, packages/docutils/rstgen, jester, qr
 randomize()
 include "index.nimf", "error.nimf"
 
